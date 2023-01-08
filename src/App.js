d97fc4f239c00d445303d7b1fb90aa9cea54af38
Atharv <atharv.n2002@gmail.com>
2023-01-08 10:50:02 +0530

    Using react-simple-code-editor

--- src/App.js
+++ src/App.js
@@ -1,24 +1,51 @@
-import logo from './logo.svg';
 import './App.css';
 
+import React, { useEffect,useState } from "react";
+// import Prism from "prismjs";
+// import "prismjs/themes/prism-tomorrow.css";
+
+import Editor from 'react-simple-code-editor';
+import { highlight, languages } from 'prismjs/components/prism-core';
+import 'prismjs/components/prism-clike';
+import 'prismjs/components/prism-javascript';
+import 'prismjs/themes/prism.css'; //Example style, you can use another
+
+
+
+const code=`function foo(){ a++; }`
+let language='javascript';
+
+
+
+
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+  // useEffect(() => {
+  //   Prism.highlightAll();
+  // }, []);
+  // return (
+  //   <div className="Code">
+  //     <pre>
+  //       <code className={`language-${language}`} ><textarea style={{background:"transparent"}}></textarea></code>
+  //     </pre>
+  //   </div>
+  // );
+
+  const [code, setCode] = useState(
+    `function add(a, b) {\n  return a + b;\n}`
+  )
+
+  return(
+   <Editor
+      value={code}
+      onValueChange={code => setCode(code)}
+      highlight={code => highlight(code, languages.js)}
+      padding={10}
+      style={{
+        fontFamily: '"Fira code", "Fira Mono", monospace',
+        fontSize: 12,
+        backgroundColor:"rgba(255,255,255)"
+      }}
+    />
   );
 }
 
